c48d4cc82c084119b99a4550c467a3bfa7ff4647
tobilg <tobilg@gmail.com>
2019-06-23 21:18:55 +0200

    Update Lambda function

--- lambda/index.js
+++ lambda/index.js
@@ -1,13 +1,23 @@
+const exec = require('child_process').exec;
+
+// Set path
 process.env['PATH'] = process.env['PATH'] + ':' + process.cwd();
 
-const exec = require('child_process').exec;
+// Set home directory
+process.env['HOME'] = '/tmp';
+
+// Set buffer size
 const MAX_OUTPUT = 1024 * 1024 * 1024; // 1 GB
-let cwd = null;
-exports.handler = function(event, context) {
-    var child = exec(`${event.command} && pwd`, {encoding: 'binary', maxBuffer: MAX_OUTPUT, cwd: cwd},
+
+// Store current working directory
+let cwd = '/tmp';
+
+exports.handler = function (event, context) {
+    const child = exec(`${event.command} && pwd`, { encoding: 'binary', maxBuffer: MAX_OUTPUT, cwd: cwd },
         function (error, stdout, stderr) {
             let newStdOut = '';
             if (!error) {
+                // Hack
                 const temp = stdout.split('\n');
                 cwd = temp[temp.length-2];
                 temp.pop();
@@ -16,11 +26,13 @@ exports.handler = function(event, context) {
             } else {
                 newStdOut = stdout;
             }
+            // Define response
             const result = {
                 "stdout": Buffer.from(`${newStdOut}`, 'binary').toString('base64'),
                 "stderr": Buffer.from(stderr, 'binary').toString('base64'),
                 "error": error
             };
+            // Send response
             context.succeed(result);
         }
     );
