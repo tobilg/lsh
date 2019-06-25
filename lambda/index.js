efc75c6d1aaeb8677bf51a8fdd7d1d40ce2b6e48
tobilg <tobilg@gmail.com>
2019-06-25 09:36:24 +0200

    Add logging & derive buffer size from Lambda memory size

--- lambda/index.js
+++ lambda/index.js
@@ -6,19 +6,26 @@ process.env['PATH'] = process.env['PATH'] + ':' + process.cwd();
 // Set home directory
 process.env['HOME'] = '/tmp';
 
-// Set buffer size
-const MAX_OUTPUT = 1024 * 1024 * 1024; // 1 GB
+// Set buffer size to half of the Lambda memory
+const MAX_OUTPUT = ((parseInt(process.env['AWS_LAMBDA_FUNCTION_MEMORY_SIZE']) * 1024 * 1024) / 2);
 
 // Store current working directory
 let cwd = '/tmp';
 
 exports.handler = function (event, context) {
+    // Add 'pwd' to each transferred command to derive the cwd
     const child = exec(`${event.command} && pwd`, { encoding: 'binary', maxBuffer: MAX_OUTPUT, cwd: cwd },
         function (error, stdout, stderr) {
+            // Log to CloudWatch
+            console.log(stdout);
+            console.log(stderr);
+            console.log(error);
+
+            // Hack for deriving cwd
             let newStdOut = '';
+            let temp = [];
             if (!error) {
-                // Hack
-                const temp = stdout.split('\n');
+                temp = stdout.split('\n');
                 cwd = temp[temp.length-2];
                 temp.pop();
                 temp.pop();
@@ -26,12 +33,14 @@ exports.handler = function (event, context) {
             } else {
                 newStdOut = stdout;
             }
+
             // Define response
             const result = {
-                "stdout": Buffer.from(`${newStdOut}`, 'binary').toString('base64'),
+                "stdout": Buffer.from(newStdOut, 'binary').toString('base64'),
                 "stderr": Buffer.from(stderr, 'binary').toString('base64'),
                 "error": error
             };
+
             // Send response
             context.succeed(result);
         }
